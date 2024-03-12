1075c0dad8a7d6c97ad94df0f95b449e70b04917
Fyras Telmini <69505085+fyrastelmini@users.noreply.github.com>
2024-03-12 18:20:46 +0000

    added basic linkedin support

--- test_app.py
+++ test_app.py
@@ -1,2 +1,19 @@
 def test_init():
     assert True == True
+
+
+from app import extract_div_content_linkedin
+
+
+def test_linkedin():
+    test_url="https://www.linkedin.com/jobs/view/3817203204/"
+    div_class = (
+        "top-card-layout__entity-info-container flex flex-wrap papabear:flex-nowrap"
+    )
+    test_output={
+                "job_title": "Data Scientist AI",
+                "company_name": "Cephalgo",
+                "location": "France",
+                "URL": test_url,
+            }
+    assert extract_div_content_linkedin(test_url,div_class) == test_output
